4dfc325943a65f2b34a4d5ae4c2d4032e3a5a122
Josh Summerhays <russiansummer@gmail.com>
2016-12-16 15:07:08 -0700

    BUILD: PATCH /user/:id

--- server/server.js
+++ server/server.js
@@ -71,32 +71,25 @@ app.delete('/user/:id', (req, res) => {
     });
 });
 
-// app.patch('/todos/:id', (req, res) => {
-//     let id = req.params.id;
-//     let body = _.pick(req.body, ['text', 'completed']);
+app.patch('/user/:id', (req, res) => {
+    let id = req.params.id;
+    let body = _.pick(req.body, ['firstName', 'lastName', 'email']);
 
-//     if(!ObjectID.isValid(id)){
-//         return res.status(404).send('not a valid id');
-//     }
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send('not a valid id');
+    }
 
-//     if(_.isBoolean(body.completed) && body.completed){
-//         body.completedAt = new Date().getTime();
-//     } else {
-//         body.completed = false;
-//         body.completedAt = null;
-//     }
-
-//     Todo.findByIdAndUpdate(id, {
-//         $set: body
-//     }, {new: true}).then((todo) => {
-//         if(!todo){
-//             return res.status(404).send();
-//         }
-//         res.send({todo});
-//     }).catch((e) => {
-//         res.status(400).send();
-//     });
-// })
+    User.findByIdAndUpdate(id, {
+        $set: body
+    }, {new: true}).then((user) => {
+        if(!user){
+            return res.status(404).send('user not found');
+        }
+        res.send({user});
+    }).catch((e) => {
+        res.status(400).send('ERROR with request');
+    });
+});
 
 app.listen(port, () => {
     console.log(`started on port ${port}`);
